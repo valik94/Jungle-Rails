02a0207cdbdae2aaa999be70c327de19e090323f
Valentin <valentin.lyash@gmail.com>
2022-02-05 19:32:47 -0500

    adding admin categories as well as ability for admin to add a new category

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   get 'about' => 'about#show'    
@@ -27,6 +28,8 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
